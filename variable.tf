d8d923f15e9a53a1f9bc09bf3aa01093208f7996
gomi_ningen <korun24276@gmail.com>
2016-10-10 19:43:53 +0900

    access_key/secret_keyを環境変数から読み出すように変更

--- variable.tf
+++ variable.tf
@@ -1,9 +1,12 @@
 provider "aws" {
-  region = "ap-northeast-1"
+  region = "${var.region}"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
 }
 
 variable "access_key" {}
 variable "secret_key" {}
+variable "region" {
+  default = "ap-northeast-1"
+}
 
