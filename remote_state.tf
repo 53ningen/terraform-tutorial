dda22ca0104ef9df1304a1d2f612bea440b1a071
gomi_ningen <korun24276@gmail.com>
2016-10-10 18:56:09 +0900

    terraform fmt

--- remote_state.tf
+++ remote_state.tf
@@ -1,9 +1,8 @@
-
 resource "aws_s3_bucket" "terraform_tutorial_tfsate" {
-    bucket = "com.github.53ningen.terraform.tutorial.tfstate" # S3 bucket 名は global で一意になるように
-    acl = "private"
-    versioning {
-        enabled = true
-    }
-}
+  bucket = "com.github.53ningen.terraform.tutorial.tfstate" # S3 bucket 名は global で一意になるように
+  acl    = "private"
 
+  versioning {
+    enabled = true
+  }
+}
