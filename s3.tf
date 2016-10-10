edca00c5021400d76ca4241a5a37dbe38cef89e4
gomi_ningen <korun24276@gmail.com>
2016-10-10 17:47:48 +0900

    S3 の状態を変更

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "terraform_tutorial" {
     bucket = "com.github.53ningen.terraform.tutorial" # S3 bucket 名は global で一意になるように
     acl = "private"
     versioning {
-        enabled = true
+        enabled = false 
     }
 }
 
